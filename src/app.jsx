192527d38955e967bcbb46ecc4bb20c4b726d4b1
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-19 15:47:06 +0100

    browserRouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './pages/Home';
-import Header from './components/Header';
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
