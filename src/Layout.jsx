839291230e73e61fe7a73858025980b3eee69bc3
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-19 13:59:34 +0100

    layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,1 +1,12 @@
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
 
+export default function Layout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      {/* <Footer /> */}
+    </div>
+  );
+}
