a5c158cdb7785f59e61e18b52050b586f5a88875
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-19 17:39:53 +0100

    main takes max space

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,15 @@
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
 
 export default function Layout() {
   return (
-    <div>
+    <div className='flex flex-col h-screen'>
       <Header />
-      <Outlet />
-      {/* <Footer /> */}
+      <main className='flex-grow'>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className='bg-slate-400 p-3'>Footer</footer>;
+}
