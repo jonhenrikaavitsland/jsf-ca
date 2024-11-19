7e0c519e46cd3949ed96fa7776d4c550b6725ef7
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-19 19:22:41 +0100

    new size and added shadow

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,10 @@
 export default function ProductCard({ product }) {
   return (
-    <div className='p-2 bg-slate-200 w-40'>
+    <div className='p-2 bg-slate-200 w-60 rounded-lg shadow-lg shadow-slate-400'>
       <div className='aspect-square bg-gray-300'>
         <img src={product.image.url} alt={product.image.alt} className='w-full h-full object-cover' />
       </div>
-      <section className='flex flex-col gap-4 p-3 text-center h-48 justify-between'>
+      <section className='flex flex-col gap-4 p-3 text-center h-36 justify-between'>
         <h2 className='font-bold'>{product.title}</h2>
         <button className='p-3 bg-orange-400 rounded-xl font-semibold hover:bg-orange-700 hover:text-white'>View Product</button>
       </section>
