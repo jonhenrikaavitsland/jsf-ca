88bd4eac12b4a8cfc57909c1af3f60f36f3a3ef6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-19 16:55:03 +0100

    cart icon

---
+++ src/components/CartIcon.jsx
@@ -0,0 +1,9 @@
+export default function CartIcon() {
+  return (
+    <div className='p-3'>
+      <button>
+        <img src='/cart-shopping-fast-solid.svg' alt='cart' className='h-8 w-8' />
+      </button>
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import CartIcon from './CartIcon';
 import NavBar from './NavBar';
 
 export default function Header() {
   return (
     <header className='bg-slate-400 pb-3 flex justify-between'>
       <NavBar />
+      <CartIcon />
     </header>
   );
 }
