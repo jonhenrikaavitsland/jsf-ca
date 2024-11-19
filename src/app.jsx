efb2b56dfc3ca5389e2aae3f951229bb37117780
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-19 15:26:16 +0100

    test

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './pages/Home';
 import Header from './components/Header';
 
 export default function App() {
   return (
-    <>
+    <div>
       <Header />
-    </>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
