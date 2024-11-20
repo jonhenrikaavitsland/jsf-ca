8ee5d572062f3b6f707b07715e6ef4452a7c943f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-20 10:12:23 +0100

    one further rearranged file

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Header/Index';
+import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer/Index';
 
