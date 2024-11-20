1e1169156bc92725d886d00af6e9a538ee7775ce
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-20 11:20:24 +0100

    includes route to individual product page

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,7 @@ export default function App() {
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />} />
+            {/* <Route path='/product/:productId' element={<ProductPage />} /> */}
           </Route>
         </Routes>
       </div>
