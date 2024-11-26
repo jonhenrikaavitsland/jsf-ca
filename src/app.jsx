2c63988c996987fc7f3b34e735800d379669d842
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-26 23:08:16 +0100

    route productpage

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './components/pages/Home';
+import ProductPage from './components/pages/ProductPage';
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />} />
-            {/* <Route path='/product/:productId' element={<ProductPage />} /> */}
+            <Route path='/product/:productId' element={<ProductPage />} />
           </Route>
         </Routes>
       </div>
