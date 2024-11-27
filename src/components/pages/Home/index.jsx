2509696035d9eb8e613a6780f23dcebc6f3005aa
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-11-27 22:53:14 +0100

    nolonger using zustand to fetch items

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,22 +1,50 @@
-import { useEffect } from 'react';
-import { useStore } from '../../../data/useStore';
+import { useEffect, useState } from 'react';
 import ProductCard from '../../ProductCard';
 import SearchBar from '../../SearchBar';
 
-export default function Home() {
-  const { products, fetchProducts } = useStore();
+function useApi(url) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts]);
+    async function getData() {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const fetchedData = await fetch(url);
+        const json = await fetchedData.json();
+        setData(json);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, [url]);
+  return { data, isLoading, isError };
+}
 
-  console.log('Store:', products);
+export default function Home() {
+  const { data, isLoading, isError } = useApi('https://v2.api.noroff.dev/online-shop');
+
+  if (isLoading) {
+    return <div className='flex justify-center'>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  console.log(data);
 
   return (
     <div className='flex flex-col gap-4'>
-      <SearchBar products={products} />
+      <SearchBar products={data.data} />
       <div className='flex flex-wrap gap-6 justify-center'>
-        {products.map((product) => (
+        {data.data?.map((product) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
