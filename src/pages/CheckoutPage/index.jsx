8e54355049c999f789a550db2569215962f52761
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-06 07:48:43 +0100

    checkout page styling

--- src/pages/CheckoutPage/index.jsx
+++ src/pages/CheckoutPage/index.jsx
@@ -1,3 +1,40 @@
+import { useReducer } from 'react';
+import { reducer, initialState } from '../../stores/cartStore';
+
 export default function CheckoutPage() {
-  return <div>cart</div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state.cart);
+
+  return (
+    <section>
+      <h1 className='font-black text-xl'>Cart:</h1>
+      <div className='border-2 p-4 flex flex-col gap-2'>
+        {state.cart.map((item) => {
+          console.log(item);
+          return (
+            <div key={item.id} className='xsm:flex gap-2'>
+              <div className='aspect-square w-20 h-20'>
+                <img src={item.image.url} alt={item.image.alt} className='w-20 h-20' />
+              </div>
+              <section className='flex flex-col'>
+                <h2>Item: {item.title}</h2>
+                <span>Quantity: {item.quantity}</span>
+                <span>Price: ${item.discountedPrice}</span>
+              </section>
+            </div>
+          );
+        })}
+        <h3 className='font-bold mt-4'>Total: ${state.total}</h3>
+        <div className='flex flex-col gap-6'>
+          <div>
+            <button className='bg-orange-600 text-white font-bold uppercase p-4 rounded-xl shadow-xl hover:bg-orange-700'>checkout</button>
+          </div>
+          <div>
+            <button className='capitalize text-gray-400 hover:text-slate-700 p-2'>clear cart</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
