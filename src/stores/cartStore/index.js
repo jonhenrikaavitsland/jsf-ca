69cd322e5fa7eb219c66d447d1fdea3b868af443
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-06 01:37:57 +0100

    persist

--- src/stores/cartStore/index.js
+++ src/stores/cartStore/index.js
@@ -1,16 +1,32 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useCartStore = create((set) => ({
-  cart: [], // store only 1 max of each item for this store
-  addToCart: (product) =>
-    set((state) => {
-      const isAlreadyInCart = state.cart.some((item) => item.id === product.id);
-      if (isAlreadyInCart) {
-        // If the product is already in the cart, do nothing
-        return state;
-      }
-      // Add product to cart if it's not already present
-      return { cart: [...state.cart, product] };
+const storage =
+  typeof window !== "undefined" && window.localStorage
+    ? window.localStorage
+    : undefined;
+
+export const useCartStore = create(
+  persist(
+    (set) => ({
+      cart: [], // Init store
+      addToCart: (product) =>
+        set((state) => {
+          const isAlreadyInCart = state.cart.some(
+            (item) => item.id === product.id,
+          );
+          if (isAlreadyInCart) {
+            // If the product is already in the cart, do nothing
+            return state;
+          }
+          // Add product to cart if it's not already present
+          return { cart: [...state.cart, product] };
+        }),
+      emptyCart: () => set({ cart: [] }), // Clear the cart
     }),
-  emptyCart: () => set({ cart: [] }), // Clear the cart
-}));
+    {
+      name: "cart-storage", //key
+      storage, // checks for localStorage
+    },
+  ),
+);
