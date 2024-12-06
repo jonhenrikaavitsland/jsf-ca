892ddb446a6e5bf742e12a412ce4c81798323611
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-06 02:45:40 +0100

    cart icon now shows number of items in the cart

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -1,8 +1,9 @@
-export default function CartIcon() {
+export default function CartIcon({ cartLength }) {
   return (
-    <div className='p-3'>
+    <div className='relative p-3'>
       <button>
         <img src='/cart-shopping-fast-solid.svg' alt='cart' className='h-8 w-8' />
+        {cartLength > 0 && <span className='absolute top-0 right-0 h-5 w-5 bg-red-600 text-white rounded-full text-xs flex items-center justify-center'>{cartLength}</span>}
       </button>
     </div>
   );
