7b563fdda42e1309a0507d694fe2450a924360b1
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-06 08:17:21 +0100

    cart can now be emptied, and clicking the cart icon navigates the user

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -1,12 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import { useCart } from '../../stores/cartStore/CartContext';
 
 export default function CartIcon() {
   const { cart } = useCart();
   const cartLength = cart.reduce((sum, item) => sum + item.quantity, 0);
 
+  const navigate = useNavigate();
+
+  function handleIconClick() {
+    navigate(`/checkout/`);
+  }
+
   return (
     <div className='relative p-3'>
-      <button>
+      <button onClick={handleIconClick}>
         <img src='/cart-shopping-fast-solid.svg' alt='cart' className='h-8 w-8' />
         {cartLength > 0 && <span className='absolute top-0 right-0 h-5 w-5 bg-red-600 text-white rounded-full text-xs flex items-center justify-center'>{cartLength}</span>}
       </button>
--- src/pages/CheckoutPage/index.jsx
+++ src/pages/CheckoutPage/index.jsx
@@ -1,17 +1,17 @@
-import { useReducer } from 'react';
-import { reducer, initialState } from '../../stores/cartStore';
+import { useCart } from '../../stores/cartStore/CartContext';
 
 export default function CheckoutPage() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { dispatch, cart, total } = useCart();
 
-  console.log(state.cart);
+  const handleClearCart = () => {
+    dispatch({ type: 'clearCart' });
+  };
 
   return (
     <section>
       <h1 className='font-black text-xl'>Cart:</h1>
       <div className='border-2 p-4 flex flex-col gap-2'>
-        {state.cart.map((item) => {
-          console.log(item);
+        {cart.map((item) => {
           return (
             <div key={item.id} className='xsm:flex gap-2'>
               <div className='aspect-square w-20 h-20'>
@@ -25,13 +25,15 @@ export default function CheckoutPage() {
             </div>
           );
         })}
-        <h3 className='font-bold mt-4'>Total: ${state.total}</h3>
+        <h3 className='font-bold mt-4'>Total: ${total.toFixed(2)}</h3>
         <div className='flex flex-col gap-6'>
           <div>
             <button className='bg-orange-600 text-white font-bold uppercase p-4 rounded-xl shadow-xl hover:bg-orange-700'>checkout</button>
           </div>
           <div>
-            <button className='capitalize text-gray-400 hover:text-slate-700 p-2'>clear cart</button>
+            <button className='capitalize text-gray-400 hover:text-slate-700 p-2' onClick={handleClearCart}>
+              clear cart
+            </button>
           </div>
         </div>
       </div>
