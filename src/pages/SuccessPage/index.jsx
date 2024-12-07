558fc369fff96598e7a480c67780641bbebb3fc3
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-07 06:28:10 +0100

    basic success page

---
+++ src/pages/SuccessPage/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function SuccessPage() {
+  return (
+    <section>
+      <h1>Success!</h1>
+      <p>Your order was successfully completed.</p>
+      <Link to='/'>Go back to the Store</Link>
+    </section>
+  );
+}
