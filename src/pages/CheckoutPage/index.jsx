22116a7e6e1778c5ba7ab368054cdb2e9e08c17d
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-07 06:34:44 +0100

    checkout button now clears cart and navigates too successpage

--- src/pages/CheckoutPage/index.jsx
+++ src/pages/CheckoutPage/index.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import { useCart } from '../../stores/cartStore/CartContext';
 
 export default function CheckoutPage() {
   const { dispatch, cart, total } = useCart();
 
+  const navigate = useNavigate();
+
   const handleClearCart = () => {
     dispatch({ type: 'clearCart' });
   };
+
+  function handleCheckoutClick() {
+    handleClearCart();
+    navigate(`/success/`);
+  }
 
   return (
     <section>
@@ -28,7 +36,9 @@ export default function CheckoutPage() {
         <h3 className='font-bold mt-4'>Total: ${total.toFixed(2)}</h3>
         <div className='flex flex-col gap-6'>
           <div>
-            <button className='bg-orange-600 text-white font-bold uppercase p-4 rounded-xl shadow-xl hover:bg-orange-700'>checkout</button>
+            <button className='bg-orange-600 text-white font-bold uppercase p-4 rounded-xl shadow-xl hover:bg-orange-700' onClick={handleCheckoutClick}>
+              checkout
+            </button>
           </div>
           <div>
             <button className='capitalize text-gray-400 hover:text-slate-700 p-2' onClick={handleClearCart}>
