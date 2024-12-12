dedb4262aee2abe8a968148b2b7b946353b17652
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-12 13:41:04 +0100

    navlink onclick

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -9,24 +9,28 @@ export default function NavBar() {
   }
 
   return (
-    <nav className='z-10'>
-      <div className='p-3'>
-        <button onClick={toggleMenu} className='h-8 w-8 lg:hidden'>
-          <img src='/burger-sharp-duotone-regular.svg' alt='menu' />
+    <nav className="z-10">
+      <div className="p-3">
+        <button onClick={toggleMenu} className="h-8 w-8 lg:hidden">
+          <img src="/burger-sharp-duotone-regular.svg" alt="menu" />
         </button>
       </div>
-      <div className={menuOpen ? 'absolute top-0 bg-slate-400 p-3' : 'hidden lg:flex'}>
-        <button onClick={toggleMenu} className='h-8 w-8 lg:hidden'>
-          <img src='/xmark-solid.svg' alt='close' />
+      <div
+        className={
+          menuOpen ? "absolute top-0 bg-slate-400 p-3" : "hidden lg:flex"
+        }
+      >
+        <button onClick={toggleMenu} className="h-8 w-8 lg:hidden">
+          <img src="/xmark-solid.svg" alt="close" />
         </button>
-        <ul className='flex flex-col gap-6 lg:flex-row p-3'>
+        <ul className="flex flex-col gap-6 lg:flex-row p-3">
           <li>
-            <NavLink to='/' className='p-2'>
+            <NavLink to="/" className="p-2" onClick={toggleMenu}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to='/contact/' className='p-2'>
+            <NavLink to="/contact/" className="p-2" onClick={toggleMenu}>
               Contact
             </NavLink>
           </li>
