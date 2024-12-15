95086fa2410ec9dc033130f04b76e72dcb9ca601
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-15 16:15:15 +0100

    test to see if i can resolve issue

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,6 @@
-import Header from "../Header/";
 import { Outlet } from "react-router-dom";
-import Footer from "../Footer/";
+import Header from "../Header";
+import Footer from "../Footer";
 
 export default function Layout() {
   return (
