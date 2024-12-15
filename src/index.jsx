01e52d38dd2213469c5f2dcd539b8c5b602f9551
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-15 16:00:39 +0100

    moved App to components folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./css/index.css";
-import App from "./App.jsx";
+import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./stores/cartStore/CartContext";
 
