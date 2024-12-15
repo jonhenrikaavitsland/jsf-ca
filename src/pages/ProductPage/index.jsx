08154784699e07b9b3b9fe6dd3fa33c2b394e9f6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-15 15:16:54 +0100

    imported reviewcard

--- src/pages/ProductPage/index.jsx
+++ src/pages/ProductPage/index.jsx
@@ -1,7 +1,8 @@
-/* eslint-disable react/prop-types */
+ 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useCart } from "../../stores/cartStore/CartContext";
+import ReviewCard from "../../components/ReviewCard";
 
 export default function ProductPage() {
   const { dispatch } = useCart();
@@ -84,39 +85,5 @@ export default function ProductPage() {
         </section>
       </div>
     </div>
-  );
-}
-
-function ReviewCard({ review }) {
-  return (
-    <div className="bg-slate-100 rounded-xl p-4 shadow-lg shadow-slate-300 border-2 flex flex-col gap-4">
-      <section className="flex flex-col gap-1">
-        <h3>Review by: {review.username}</h3>
-        <IconDisplay rating={review.rating} Icon={RatingIcon} />
-      </section>
-      <p>{review.description}</p>
-    </div>
-  );
-}
-
-function IconDisplay({ rating, Icon }) {
-  return (
-    <div className="flex gap-1">
-      {Array.from({ length: rating }, (_, index) => (
-        <Icon key={index} />
-      ))}
-    </div>
-  );
-}
-
-function RatingIcon() {
-  return (
-    <span>
-      <img
-        src="/cart-shopping-fast-solid.svg"
-        alt="rating icon"
-        className="h-5 w-5"
-      />
-    </span>
   );
 }
