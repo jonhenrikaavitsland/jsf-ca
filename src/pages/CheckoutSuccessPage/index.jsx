b08452b615b860f30fa256e9a446e75707e79b06
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-17 18:06:57 +0100

    adding styling to the success checkout page

--- src/pages/CheckoutSuccessPage/index.jsx
+++ src/pages/CheckoutSuccessPage/index.jsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function CheckoutSuccessPage() {
   return (
-    <section>
-      <h1>Success!</h1>
+    <section className="flex flex-col gap-2 items-center">
+      <h1 className="font-bold text-6xl uppercase">Success!</h1>
       <p>Your order was successfully completed.</p>
-      <Link to='/'>Go back to the Store</Link>
+      <Link to="/" className="text-slate-800 font-semibold">
+        🔙 Go back to the Store
+      </Link>
     </section>
   );
 }
