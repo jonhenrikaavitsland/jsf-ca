8f2093360c9b947651b0ac457ed3c640f56de358
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-17 18:40:10 +0100

    minor styling

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -1,5 +1,5 @@
-import { useNavigate } from 'react-router-dom';
-import { useCart } from '../../stores/cartStore/CartContext';
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../../stores/cartStore/CartContext";
 
 export default function CartIcon() {
   const { cart } = useCart();
@@ -12,10 +12,18 @@ export default function CartIcon() {
   }
 
   return (
-    <div className='relative p-3'>
+    <div className="relative m-3 p-3">
       <button onClick={handleIconClick}>
-        <img src='/cart-shopping-fast-solid.svg' alt='cart' className='h-8 w-8' />
-        {cartLength > 0 && <span className='absolute top-0 right-0 h-5 w-5 bg-red-600 text-white rounded-full text-xs flex items-center justify-center'>{cartLength}</span>}
+        <img
+          src="/cart-shopping-fast-solid.svg"
+          alt="cart"
+          className="h-8 w-8"
+        />
+        {cartLength > 0 && (
+          <span className="absolute top-0 right-0 h-5 w-5 bg-red-600 text-white rounded-full text-xs flex items-center justify-center">
+            {cartLength}
+          </span>
+        )}
       </button>
     </div>
   );
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -10,7 +10,7 @@ export default function NavBar() {
 
   return (
     <nav className="z-10">
-      <div className="p-3">
+      <div className="p-3 m-3">
         <button onClick={toggleMenu} className="h-8 w-8 lg:hidden">
           <img src="/burger-sharp-duotone-regular.svg" alt="menu" />
         </button>
@@ -20,10 +20,10 @@ export default function NavBar() {
           menuOpen ? "absolute top-0 bg-slate-400 p-3" : "hidden lg:flex"
         }
       >
-        <button onClick={toggleMenu} className="h-8 w-8 lg:hidden">
+        <button onClick={toggleMenu} className="h-8 w-8 m-3 lg:hidden">
           <img src="/xmark-solid.svg" alt="close" />
         </button>
-        <ul className="flex flex-col gap-6 lg:flex-row p-3">
+        <ul className="flex flex-col gap-6 mt-6 lg:mt-0 lg:flex-row p-3">
           <li>
             <NavLink to="/" className="p-2" onClick={toggleMenu}>
               Home
