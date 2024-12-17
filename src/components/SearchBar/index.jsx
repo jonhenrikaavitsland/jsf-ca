e2a958b45681dd4e34f402d2563f21c8dd2ce69e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-12-17 18:00:03 +0100

    label added

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar({ products }) {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const navigate = useNavigate();
 
@@ -11,13 +12,15 @@ export default function SearchBar({ products }) {
     setSearchTerm(input);
 
     // Clear matches if the input is empty
-    if (input.trim() === '') {
+    if (input.trim() === "") {
       setFilteredProducts([]);
       return;
     }
 
     // Filter products based on the input
-    const matches = products.filter((product) => product.title.toLowerCase().includes(input.toLowerCase()));
+    const matches = products.filter((product) =>
+      product.title.toLowerCase().includes(input.toLowerCase()),
+    );
     setFilteredProducts(matches);
   };
 
@@ -26,13 +29,31 @@ export default function SearchBar({ products }) {
   }
 
   return (
-    <div className='relative w-2/3 mx-auto'>
-      <input className='w-full p-2 rounded-xl border-4 border-slate-400' type='text' placeholder='Search for products...' value={searchTerm} onChange={handleInputChange} />
+    <div className="relative w-2/3 mx-auto">
+      <label htmlFor="search" className="sr-only">
+        Search our products
+      </label>
+      <input
+        className="w-full p-2 rounded-xl border-4 border-slate-400"
+        type="text"
+        placeholder="Search for products..."
+        value={searchTerm}
+        onChange={handleInputChange}
+        id="search"
+      />
       {filteredProducts.length > 0 && (
-        <ul className='absolute top-full inset-x-0 bg-white border-[1px] z-10 p-0 m-0'>
+        <ul className="absolute top-full inset-x-0 bg-white border-[1px] z-10 p-0 m-0">
           {filteredProducts.map((product) => (
-            <li className='p-2.5 cursor-pointer border-b-[1px] border-slate-400 flex gap-3 items-end hover:bg-slate-200' key={product.id} onClick={() => handleProductClick(product.id)}>
-              <img src={product.image.url} alt={product.image.alt} className='size-20 object-cover' />
+            <li
+              className="p-2.5 cursor-pointer border-b-[1px] border-slate-400 flex gap-3 items-end hover:bg-slate-200"
+              key={product.id}
+              onClick={() => handleProductClick(product.id)}
+            >
+              <img
+                src={product.image.url}
+                alt={product.image.alt}
+                className="size-20 object-cover"
+              />
               <span>{product.title}</span>
             </li>
           ))}
