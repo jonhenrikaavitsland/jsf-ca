1adf384f0af6ac9fc30fd8f57d8d68cb2a09773b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-28 11:08:49 +0100

    logo added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,11 +8,23 @@ export default function Header() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <header className="bg-slate-400 pb-3 flex justify-between">
+    <header className="bg-slate-400 px-5 flex items-center">
+      <Logo />
       <NavBar />
       <CartIcon
         cartLength={state.cart.reduce((sum, item) => sum + item.quantity, 0)}
       />
     </header>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="flex items-end">
+      <img src="/cart-shopping-fast-solid.svg" alt="logo" className="h-14" />
+      <span className="capitalize font-black font-serif italic text-4xl">
+        quick shop
+      </span>
+    </div>
   );
 }
