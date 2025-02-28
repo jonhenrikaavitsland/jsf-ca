5f553d0e13706b06f6eb3771313a53e1f120ec05
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-28 11:21:20 +0100

    updated footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer className='bg-slate-400 p-3'>Footer</footer>;
+  return (
+    <footer className="bg-slate-400 p-3 text-center capitalize">
+      <p>© 2025 quick shop. all rights reserved.</p>
+    </footer>
+  );
 }
